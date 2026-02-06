33dc5413f88a5cc784f3198b3c94a868dfd0b573
Daniel <daniel.hofmann1@iu-study.org>
2026-02-06 13:39:48 +0100

    Spendenseite erstellt und Vue.js Formular-Logik implementiert

---
+++ scripts.js
@@ -0,0 +1,43 @@
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+        return {
+            uebergabeArt: 'abholung',
+            adresse: {
+                name: '',
+                vorname: '',
+                strasse: '',
+                plz: '',
+                stadt: ''
+            },
+            spende: {
+                kleidungsart: '',
+                krisengebiet: ''
+            }
+        }
+    },
+
+    methods: {
+        registrieren() {
+            if (this.spende.kleidungsart === '' || this.spende.krisengebiet === '') {
+                alert("Bitte wähle eine Kleidungsart und ein Krisengebiet aus!");
+                return;
+            }
+
+            if (this.uebergabeArt === 'abholung') {
+                if (this.adresse.name === '' || this.adresse.vorname === '' || this.adresse.strasse === '' || this.adresse.plz === '' || this.adresse.stadt === '') {
+                    alert("Bitte gib deine vollständige Adresse an!");
+                    return;
+                }
+                if (!this.adresse.plz.startsWith('64')) {
+                    alert("Wir holen leider nur im Bereich 64xxx ab. Bitte bringen Sie die Spende vorbei");
+                    return;
+                }
+            }
+        }
+
+    }
+
+}).mount('#app');
